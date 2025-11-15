c248cd82e530836d75f162e8b1501a0c2d3148a0
codemathsz <mo284891@gmail.com>
2025-11-15 17:27:20 -0300

    feat: adjust styles Nav Item

--- src/components/AppNavItem/index.tsx
+++ src/components/AppNavItem/index.tsx
@@ -1,6 +1,6 @@
 import type { LucideIcon } from 'lucide-react';
 import type { RedirectTo } from '../../routes';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 interface AppNavItemProps {
     label: string;
@@ -9,9 +9,14 @@ interface AppNavItemProps {
 }
 
 export function AppNavItem({ label, icon: Icon, redirectTo }: AppNavItemProps){
+
+    const location = useLocation()
+    
+    const isActive = (path: string) => location.pathname === path;
+
     return(
         <Link 
-            className={`bg-background px-3 py-2 rounded-lg flex gap-2 items-center justify-center cursor-pointer hover:bg-[#3b82f6] hover:text-white transition-colors`}
+            className={`bg-background px-3 py-2 rounded-lg flex gap-2 items-center justify-center cursor-pointer hover:bg-[#3b82f6] hover:text-white transition-colors ${isActive(redirectTo) ? 'bg-[#3b82f6] text-white' : ''}`}
             to={redirectTo}
         >
             <Icon className="h-4 w-4 text-primary" />

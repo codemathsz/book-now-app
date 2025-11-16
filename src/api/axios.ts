2f11f5ced413474251ecf8a11fd0100767e78219
codemathsz <mo284891@gmail.com>
2025-11-16 13:30:06 -0300

    firebase

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = 'http://localhost:3000/api'
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000/aapi';
 
 export const axiosInstance = axios.create({
     baseURL,
